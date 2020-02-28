823f3f7309b5475a4dee4e6946f2ae1b20ecf33a
Gopi Gongalareddy <siribasireddi@gmail.com>
2020-02-28 18:29:57 -0500

    added app.js file

---
+++ mern-crud/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+
+const book = require("./routes/book");
+const app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: "false" }));
+app.use(express.static(path.join(__dirname, "build")));
+
+app.use("/api/book", book);
+
+//catch 404 and forward to error handling
+
+app.use(function(req, res, next) {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+//error handler
+app.use(function(err, req, res, next) {
+  //set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+export default app;
