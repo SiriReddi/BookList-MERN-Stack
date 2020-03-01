907612d3163a9a48c0eff277d301d1e043d5b591
Gopi Gongalareddy <siribasireddi@gmail.com>
2020-03-01 17:03:46 -0500

    added all restful api routes

--- mern-crud/routes/book.js
+++ mern-crud/routes/book.js
@@ -1,8 +1,46 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const Book = require("../models/Book.js");
 
-/* GET home page */
+/* GET All Books */
 router.get("/", function(req, res, next) {
-  res.send("Express REST API");
+  Book.find(function(err, products) {
+    if (err) return next(err);
+    res.json(products);
+  });
 });
+
+/*Get single Book by Id */
+router.get("/:id", function(req, res, next) {
+  Book.findById(req.params.id, function(err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/*Save Book */
+router.post("/", function(req, res, next) {
+  Book.create(req.body, function(err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/*Update Book */
+router.put(":/id", function(req, res, next) {
+  Book.findByIdAndUpdate(req.params.id, req.body, function(err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/*Delete Book */
+router.delete(":/id", function(req, res, next) {
+  Book.findByIdAndRemove(req.params.id, req.body, function(err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
 module.exports = router;
